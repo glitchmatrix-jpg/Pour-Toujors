9b9e975075a6a7ef6b1024ba52fd6e89de61f490
glitchmatrix-jpg <extraextra335@gmail.com>
2026-08-02 19:08:20 +0500

    Add living Today theme token aliases

---
+++ lib/core/weather/weather_models_theme_aliases.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import '../../app/theme/pour_toujours_theme.dart';
+
+extension LivingTodayThemeAliases on PtThemeTokens {
+  Color get accent => globeAccent;
+  Color get surface => elevated;
+}
