ecc7db5cbefebd238a2ab350fc7c9ac27b42cf4a
glitchmatrix-jpg <extraextra335@gmail.com>
2026-08-02 06:07:46 -0500

    feat: initialize timezone database

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
 import 'app/pour_toujours_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  tz.initializeTimeZones();
   runApp(const ProviderScope(child: PourToujoursApp()));
 }
