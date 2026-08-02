ec3b21a89b5d9d09a8c2e8a8ebdf0eef54186c3b
glitchmatrix-jpg <extraextra335@gmail.com>
2026-08-02 21:40:54 +0500

    feat: initialize v1 platform services

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:timezone/data/latest.dart' as tz;
 
 import 'app/pour_toujours_app.dart';
+import 'core/release/v1_platform_services.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   tz.initializeTimeZones();
+  await V1PlatformServices.instance.initialize();
   runApp(const ProviderScope(child: PourToujoursApp()));
 }
