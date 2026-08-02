c4ad6e68aaee966cda01d93b94d98a2e9acfdd70
glitchmatrix-jpg <extraextra335@gmail.com>
2026-08-02 18:33:39 +0500

    Wire persistent themes and deep routes

--- lib/app/pour_toujours_app.dart
+++ lib/app/pour_toujours_app.dart
@@ -1,18 +1,51 @@
 import 'package:flutter/material.dart';
 
+import '../core/settings/app_settings.dart';
+import '../features/navigation/detail_placeholders.dart';
 import '../features/onboarding/identity_gate.dart';
 import 'theme/pour_toujours_theme.dart';
 
-class PourToujoursApp extends StatelessWidget {
+class PourToujoursApp extends StatefulWidget {
   const PourToujoursApp({super.key});
 
   @override
+  State<PourToujoursApp> createState() => _PourToujoursAppState();
+}
+
+class _PourToujoursAppState extends State<PourToujoursApp> {
+  final AppSettingsController _settings = AppSettingsController();
+
+  @override
+  void initState() {
+    super.initState();
+    _settings.load();
+  }
+
+  @override
+  void dispose() {
+    _settings.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pour Toujours',
-      debugShowCheckedModeBanner: false,
-      theme: PourToujoursTheme.light,
-      home: const IdentityGate(),
+    return AppSettingsScope(
+      controller: _settings,
+      child: AnimatedBuilder(
+        animation: _settings,
+        builder: (context, _) {
+          final value = _settings.value;
+          return MaterialApp(
+            title: 'Pour Toujours',
+            debugShowCheckedModeBanner: false,
+            themeMode: value.themeMode,
+            theme: PourToujoursTheme.build(value.theme, Brightness.light),
+            darkTheme: PourToujoursTheme.build(value.theme, Brightness.dark),
+            home: const IdentityGate(),
+            onGenerateRoute: PourToujoursRoutes.onGenerateRoute,
+          );
+        },
+      ),
     );
   }
 }
